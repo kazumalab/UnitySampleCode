8ced3aa4c88774081af493ac46c690ba1e6c315c
Kazumalab <kazuma-arashi@hotmail.co.jp>
2016-05-28 03:57:45 +0900

    Add Script

---
+++ RigidbodySample.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RigidbodySample : MonoBehaviour {
+
+	private Rigidbody rb;
+	private float power = 10.0f;
+
+	void Start () {
+		
+		// 必ずゲームオブジェクトにRigidbodyをつけ、getcomponentする。
+		rb = this.GetComponent<Rigidbody> ();
+	}
+
+	void Update () {
+		
+		rb.AddForce (new Vector3 (power, 0, 0)); // Updateごとにpower分のちからを加える。
+
+		rb.AddTorque (new Vector3 (power, 0, 0)); // Updateごとにpower分の回転を加える
+	}
+}
---
+++ TransformSample.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TransformSample : MonoBehaviour {
+
+	void Update () {
+		// InputSample.csと組み合わせる
+		float dx = Input.GetAxis("Horizontal");
+		float dz = Input.GetAxis ("Vertical");
+
+		// キーボードの入力に合わせて移動
+		this.transform.position = new Vector3 (this.transform.position.x + dx, this.transform.position.y, this.transform.position.z + dz);
+
+		this.transform.Translate (new Vector3 (this.transform.position.x + dx, this.transform.position.y, this.transform.position.z + dz));
+
+		// キーボードの入力に合わせて回転
+		this.transform.rotation = Quaternion.Euler (this.transform.position.x + dx, this.transform.position.y, this.transform.position.z + dz);
+
+		this.transform.Rotate (new Vector3 (this.transform.position.x + dx, this.transform.position.y, this.transform.position.z + dz));
+
+	}
+}
